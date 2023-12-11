e6f33d4b8bfee87cded495b4da25fa95ad6152b3
Edouard <116021091+Kitkatroller@users.noreply.github.com>
2023-12-11 09:04:32 +0100

    Start

---
+++ ReservatieBeheer.BL/Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservatieBeheer.BL.Models
+{
+    public class Restaurant
+    {
+        public string Naam { get; set; }
+        public string Email { get; set; }
+        public string TelefoonNummer { get; set; }
+        public Locatie Locatie { get; set; }
+        public int KlantenNummer { get; set; }
+    }
+}
---
+++ ReservatieBeheer.DL/Repositories/LocatieRepo.cs
@@ -0,0 +1,13 @@
+﻿using ReservatieBeheer.BL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservatieBeheer.DL.Repositories
+{
+    internal class LocatieRepo : ILocatieRepo
+    {
+    }
+}
